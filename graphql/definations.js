6e2f3007d73affd73a47fa6c89193e1ed78be81d
zacker-222 <64682140+zacker-222@users.noreply.github.com>
2023-12-10 15:54:20 -0800

    init

---
+++ graphql/definations.js
@@ -0,0 +1,115 @@
+import {getDatabase} from "../database/database.js"
+
+
+// A schema is a collection of type definitions (hence "typeDefs")
+// that together define the "shape" of queries that are executed against
+// your data.
+export const typeDefs = `#graphql
+  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+
+    # The "Stop" type defines the queryable fields for every stop in our data source.
+    type Stop {
+        stpid: ID!,
+        stpnm: String,
+        geoid: String,
+        lat: Float,
+        lon: Float,
+    }
+
+    # The "StopData" type defines the queryable fields for the stop data of a route in our data source.
+    type StopData {
+        dir1: String,
+        dest1: String,
+        stops1: [Stop],
+        dir2: String,
+        dest2: String,
+        stops2: [Stop]
+    }
+
+    # The "Route" type defines the queryable fields for every route in our data source.
+    type Route {
+        rt: ID!,
+        rtnm: String,
+        rtclr: String,
+        rtdd: String,
+        dir1: String,
+        stops1: [Stop],
+        dir2: String,
+        stops2: [Stop],
+        dest1: String,
+        dest2: String,
+    }
+
+    # The "Query" type is special: it lists all of the available queries that
+    # clients can execute, along with the return type for each. In this
+    # case, the "stops" query returns an array of zero or more Stops (defined above).
+    type Query {
+        stops: [Stop],
+        routes: [Route],
+        stop(stpid: ID!): Stop,
+        route(rt: ID!): Route
+    }
+`;
+
+
+export const resolvers = {
+    Query: {
+        stops: async (parent, args, context, info) => {
+            const database = context.database;
+            const collection = database.collection('stops');
+            let stops = await collection.find().toArray();
+            return stops;
+        },
+        routes: async (parent, args, context, info) => {
+            const database = context.database;
+            const collection = await database.collection('routes');
+            let routes = await collection.find().toArray();
+            return routes;
+        },
+        stop: async (parent, args, context, info) => {
+            const database = context.database;
+            const collection = await database.collection('stops');
+            let stop = await collection.findOne({stpid: args.stpid});
+            return stop;
+        },
+        route: async (parent, args, context, info) => {
+            const database = context.database;
+            const collection = await database.collection('routes');
+            let route = await collection.findOne({rt: args.rt});
+            return route;
+        }
+    },
+    Route: {
+        dest1: async (parent, args, context, info) => {
+            return parent.stopdata.dest1;
+        },
+        dest2: async (parent, args, context, info) => {
+            return parent.stopdata.dest2;
+        },
+        dir1: async (parent, args, context, info) => {
+            return parent.stopdata.dir1;
+        },
+        dir2: async (parent, args, context, info) => {
+            return parent.stopdata.dir2;
+        },
+        stops1: async (parent, args, context, info) => {
+            const database = context.database;
+            const collection = await database.collection('stops');
+            console.log(parent.stopdata.stops1);
+            console.log(await collection.find({stpid: {$in: [
+                "53000",
+                "55965",
+                "52752",
+            ]}}).toArray());
+
+            let stops = await collection.find({stpid: {$in: parent.stopdata.stops1}}).toArray();
+            console.log(parent.stopdata.stops1, [
+                "53000",
+                "55965",
+                "52752",
+            ]);
+            return stops;
+        },
+
+    }
+};
---
+++ index.js
@@ -0,0 +1,35 @@
+import {connectToDatabase, getDatabase} from './database/database.js';
+import express from 'express';
+import {updateDB} from './cronjobs/updateDB.js';
+import {typeDefs, resolvers} from './graphql/definations.js';
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 8080;
+const __dirname = process.cwd();
+app.use(express.json());
+
+await connectToDatabase();
+
+
+// await updateDB();
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+});
+
+const { url } = await startStandaloneServer(server, {
+    listen: { port: 4000 },
+    context: async ({ req }) => {
+        return { database: getDatabase() };
+    },
+});
+
+console.log(`🚀  Server ready at: ${url}`);
+
