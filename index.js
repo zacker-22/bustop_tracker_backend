579c136dbd8582d87102bd572431e0a3c52e5df9
zacker-222 <64682140+zacker-222@users.noreply.github.com>
2023-12-14 17:06:24 -0800

    removed express

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
 import {connectToDatabase, getDatabase} from './database/database.js';
-import express from 'express';
 import {updateDB} from './cronjobs/updateDB.js';
 import {typeDefs, resolvers} from './graphql/definations.js';
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 import cron from 'node-cron';
-import cors from 'cors';
+
 
 
 import dotenv from 'dotenv';
 dotenv.config();
 
-const app = express();
 const port = process.env.PORT || 3000
 const __dirname = process.cwd();
-app.use(cors());
-app.use(express.json());
 
 await connectToDatabase();
 
